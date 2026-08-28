3088e78baccf278c002e31c99e27824c75c05161
OrionTheProgrammer <orionpedrazauneverso@gmail.com>
2026-08-28 02:07:12 -0400

    build: integra gRPC SQLite y Swagger en catálogo

--- catalog-service/src/main/java/cl/cachaelprecio/catalog/Application.java
+++ catalog-service/src/main/java/cl/cachaelprecio/catalog/Application.java
@@ -1,7 +1,15 @@
 package cl.cachaelprecio.catalog;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Catalog Service API",
+                version = "0.1.0"
+        )
+)
 public final class Application {
 
     private Application() {
